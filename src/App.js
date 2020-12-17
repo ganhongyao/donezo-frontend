45ea10d0e75903058002bd0bf0fc362a6853dbdf
ganhongyao <ganhongyao@u.nus.edu>
2020-12-17 10:23:13 +0800

    Installed Material-UI and edited theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './Components/Header';
+import MiddleContainer from './Components/MiddleContainer'
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    background: 'linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5))',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/bg.png'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    fontFamily: 'Nunito',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+
+}))
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <MiddleContainer />
+
     </div>
   );
 }
-
-export default App;
