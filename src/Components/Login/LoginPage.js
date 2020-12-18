6f7d1d0b36cb1455d3c21f72e1895626a1df8eae
ganhongyao <ganhongyao@u.nus.edu>
2020-12-18 19:36:15 +0800

    Modified login page

--- src/Components/Login/LoginPage.js
+++ src/Components/Login/LoginPage.js
@@ -1,16 +1,99 @@
 import React from 'react';
 import { makeStyles} from '@material-ui/core/styles';
+import { Avatar, Button, Checkbox, CssBaseline, FormControlLabel, Grid, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+        marginLeft: '10%',
+        marginRight: '10%',
+        padding: '2%'
+        
+    },
+
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+
+    form: {
+        width: '40%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+        
+    },
+
+    textfield: {
+        height: '50%'
+    }, 
+
 
 }));
 
-export default function LoginPage() {
+export default function ComponentName() {
     const classes = useStyles();
 
     return (
-        <div>
-            Login page
+        <div className={classes.root}>
+            <CssBaseline />
+            <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <form className={classes.form} noValidate>
+            <TextField className={classes.textfield}
+                size="small"
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+            />
+            <TextField
+                size="small"
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+            />
+            <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+            >
+                Sign In
+            </Button>
+            <Grid container>
+                <Grid item xs>
+                <Link href="#" variant="body2">
+                    Forgot password?
+                </Link>
+                </Grid>
+                <Grid item>
+                <Link to="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                </Link>
+                </Grid>
+            </Grid>
+            </form>
         </div>
     )
 }
