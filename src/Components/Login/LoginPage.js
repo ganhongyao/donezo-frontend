e491788443c46afadfd4a29e4113bf98e869b3e6
ganhongyao <ganhongyao@u.nus.edu>
2020-12-23 17:29:40 +0800

    Changed LoginPage.js to class component

--- src/Components/Login/LoginPage.js
+++ src/Components/Login/LoginPage.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { makeStyles} from '@material-ui/core/styles';
-import { Avatar, Button, Checkbox, CssBaseline, FormControlLabel, Grid, TextField } from '@material-ui/core';
+import { Avatar, Button, Checkbox, CssBaseline, FormControlLabel, Grid, TextField, withStyles } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
     root: {
         display: 'flex',
         flexDirection: 'column',
@@ -41,70 +41,102 @@ const useStyles = makeStyles((theme) => ({
     }
 
 
-}));
+});
 
-export default function LoginPage(props) {
-    const classes = useStyles();
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          email: "",
+          password: "",
+          loginErrors: ""
+        }
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <Avatar className={classes.avatar}>
-                <LockOutlinedIcon />
-            </Avatar>
-            LOG IN
-            <form className={classes.form} noValidate>
-                <TextField className={classes.textfield}
-                    size="small"
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                    size="small"
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}  
-                    onClick={props.onSubmit}
-                >
-                    LOG IN
-                </Button>
-                <Grid container>
-                    <Grid item xs>
-                    <Link href="#" variant="body2">
-                        Forgot password?
-                    </Link>
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        const { email, password } = this.state;
+        event.preventDefault();
+    }
+
+    render() {
+
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                LOG IN
+                <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
+                    <TextField className={classes.textfield}
+                        size="small"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        type="email"
+                        autoFocus
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <TextField
+                        size="small"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}  
+                    >
+                        LOG IN
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                        <Link href="#" variant="body2">
+                            Forgot password?
+                        </Link>
+                        </Grid>
+                        <Grid item>
+                        <Link to="/signup" variant="body2">
+                            {"Don't have an account? Sign Up"}
+                        </Link>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                    <Link to="/signup" variant="body2">
-                        {"Don't have an account? Sign Up"}
-                    </Link>
-                    </Grid>
-                </Grid>
-            </form>
-        </div>
-    )
-}
+                </form>
+            </div>
+        )
+    }
+} 
+
+export default withStyles(useStyles)(LoginPage);
