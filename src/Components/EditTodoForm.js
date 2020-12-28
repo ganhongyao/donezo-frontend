a79adfc4eef6d25271a9c7c2a70d039977d07af8
ganhongyao <ganhongyao@u.nus.edu>
2020-12-28 14:49:48 +0800

    Added edit function to items

---
+++ src/Components/EditTodoForm.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { FormControl, Grid, IconButton, InputLabel, makeStyles, MenuItem, Select } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from '@date-io/date-fns';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+  },
+
+  createbutton: {
+    '&:hover': {
+      backgroundColor: '#00a152',
+    },
+  },
+
+  select: {
+    fontSize: '0.5em'
+  },
+
+  button: {
+    marginTop: '8%',
+    marginLeft: '15%'
+  }
+
+
+
+
+}));
+
+
+export default function EditTodoForm(props) {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+ 
+
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={handleClickOpen} className={classes.button}>
+        <EditIcon />
+      </IconButton>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="createform" maxWidth="xs">
+        <form onSubmit={props.handleEdit}>  
+        <DialogTitle id="createform">Edit task</DialogTitle>
+        <DialogContent>
+          <TextField
+            name="title"
+            autoFocus
+            margin="dense"
+            id="title"
+            label="Title"
+            fullWidth
+            required
+            variant="filled"
+            onChange={props.handleChange}
+            defaultValue={props.todo.title}
+          />
+
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardDatePicker
+                  autoOk
+                  id="duedate"
+                  variant="inline"
+                  label="Due Date"
+                  format="MM/dd/yyyy"
+                  InputAdornmentProps={{ position: "start" }}
+                  onChange={props.handleDateChange}
+                  value={props.defaultDate}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl className={classes.formControl}>
+                <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+                  Tag
+                  </InputLabel>
+                <Select
+                  labelId="demo-simple-select-placeholder-label-label"
+                  id="demo-simple-select-placeholder-label"
+                  name='newTag'
+                  value={props.defaultTag}
+                  onChange={props.handleChange}
+                  displayEmpty
+                  className={classes.selectEmpty}
+                >
+                  <MenuItem value="None">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={'School'}>School</MenuItem>
+                  <MenuItem value={'Fitness'}>Fitness</MenuItem>
+                  <MenuItem value={'Misc'}>Misc</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <TextField
+            autoFocus
+            margin="dense"
+            id="description"
+            label="Description"
+            fullWidth
+            variant="filled"
+          />
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button type="submit" color="primary" variant="contained" onClick={handleClose} className={classes.createbutton}>
+            Edit
+          </Button>
+        </DialogActions>
+        </form>
+        
+      </Dialog>
+    </div>
+  );
+}
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles} from '@material-ui/core/styles';
-import { IconButton, ListItem, TableCell, TableRow } from '@material-ui/core';
+import { IconButton, ListItem, TableCell, TableRow, withStyles } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
+import EditTodoForm from './EditTodoForm';
+import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
     root: {
         backgroundColor: 'rgba(255, 255, 255, 0.6)',  
         lineHeight: '10px',
@@ -16,34 +18,81 @@ const useStyles = makeStyles((theme) => ({
     cell: {
         lineHeight: '10px',
         padding: '0px',
-        paddingLeft: '15px'
+        paddingLeft: '15px',
+    },
 
+    actionscell: {
+        display: 'flex'
     }
-}));
+});
 
-
-
-export default function TodoItem(props) {
-    const classes = useStyles();
-
-    const handleDelete = () => {
-        props.handleDelete(props.todo.id)
+class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.todo.title,
+            duedate: this.props.todo.duedate
+        }
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
     }
 
-    const handleBlur = () => {
-        
+    handleDelete() {
+        this.props.handleDelete(this.props.todo.id)
     }
 
-    return (
-        <TableRow className={classes.root}>
-            <TableCell className={classes.cell}>{props.todo.title}</TableCell>
-            <TableCell className={classes.cell}>{props.todo.duedate}</TableCell>
-            <TableCell className={classes.cell}>{props.todo.tag}</TableCell>
-            <TableCell className={classes.cell} align='center'>
-                <IconButton aria-label="edit"><EditIcon /></IconButton>
-                <IconButton aria-label="delete" onClick={handleDelete}><DeleteIcon /></IconButton>
-                <IconButton aria-label="done" ><DoneOutlineIcon /></IconButton>
-            </TableCell>
-        </TableRow>
-    )
+    handleEdit() {
+        const todo = {title: this.state.title, duedate: this.state.duedate }
+        axios.put(
+        `http://localhost:3001/api/v1/todos/${this.props.todo.id}`,
+        {todo: todo}
+        )
+        .then(response => {
+        console.log(response)
+        this.props.handleUpdate(response.data)
+        })
+        .catch(error => console.log(error))
+    }
+
+    handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+    }
+    
+    handleDateChange(event, date) {
+		const year = date.split("/")[2];
+		const day = date.split("/")[1];
+		const month = date.split("/")[0];
+		const adjustedDate = year + "-" + month + "-" + day;
+		this.setState({
+			duedate: adjustedDate
+		})
+    }
+    
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <TableRow className={classes.root}>
+                <TableCell className={classes.cell}>{this.props.todo.title}</TableCell>
+                <TableCell className={classes.cell}>{this.props.todo.duedate}</TableCell>
+                <TableCell className={classes.cell}>{this.props.todo.tag}</TableCell>
+                <TableCell className={classes.actionscell} align='center'>
+                    <EditTodoForm 
+                        todo={this.props.todo} 
+                        handleChange={this.handleChange} 
+                        handleEdit={this.handleEdit} 
+                        handleDateChange={this.handleDateChange} 
+                        defaultDate={this.state.duedate}/>
+                    <IconButton aria-label="delete" onClick={this.handleDelete}><DeleteIcon /></IconButton>
+                    <IconButton aria-label="done" ><DoneOutlineIcon /></IconButton>
+                </TableCell>
+            </TableRow>
+        )
+    }
 }
+
+export default withStyles(useStyles)(TodoItem);
