8ecac6213c3b6dafd435cf05f8a79045e04e6f5f
ganhongyao <ganhongyao@u.nus.edu>
2021-01-01 20:28:35 +0800

    Fixed issue when 1 January appears as 1th January

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -60,7 +60,7 @@ export default function Header(props) {
 	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
 	dayOfWeek = weekday[objToday.getDay()],
 	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
-	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate()  : objToday.getDate() + domEnder,
+	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate()  : objToday.getDate(),
 	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
 	curMonth = months[objToday.getMonth()],
     curYear = objToday.getFullYear();
