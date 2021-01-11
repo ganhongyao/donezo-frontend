6e18b61ce0bb9ea6a0fc7460ad8e3f9595935ef6
ganhongyao <ganhongyao@u.nus.edu>
2021-01-11 23:05:37 +0800

    Allow users to check off task from dialog

--- src/Components/ItemDialog.js
+++ src/Components/ItemDialog.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Button, Chip } from '@material-ui/core';
+import axios from 'axios';
+import { toast } from 'react-toastify'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,13 +30,42 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '10px',
         backgroundColor: '#54e346',
     },
+
+    actions: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+
+    donezo: {
+        '&:hover': {
+            backgroundColor: '#00a152',
+        },
+    }
 }));
+
+
 
 export default function ItemDialog(props) {
     const classes = useStyles();
 
+    const handleComplete = () => {
+        const todo = {completed: !props.todo.completed}
+            axios.put(
+            `http://localhost:3001/api/v1/todos/${props.todo.id}`,
+            {todo: todo}
+            )
+            .then(response => {
+            console.log(response)
+            props.handleUpdate(response.data)
+            })
+            .catch(error => console.log(error))
+            !props.todo.completed && 
+                toast.success('Donezo!', 
+                {
+                    position: toast.POSITION.TOP_CENTER,
+                })
+    }
     
-
     return (
         <Dialog fullWidth open={props.open} onClose={props.handleClose} aria-labelledby="createform" maxWidth="md" className={classes.root}>
             <DialogTitle id="createform">{props.todo.title}</DialogTitle>
@@ -42,7 +73,6 @@ export default function ItemDialog(props) {
                 <div className={classes.field}>
                     <img src={process.env.PUBLIC_URL + '/calendar.png'} className={classes.icon}/>
                     {props.todo.duedate} {" "} 
-                    ( {props.daysleft} )<br/>  
                 </div>
                 <div className={classes.field}>
                     <img src={process.env.PUBLIC_URL + '/description.png'} className={classes.icon}/>
@@ -55,8 +85,11 @@ export default function ItemDialog(props) {
                 </div>
                 
             </DialogContent>
-            <DialogActions>
-                <Button onClick={props.handleClose} color="primary" style={{margin: '0 auto'}}>
+            <DialogActions className={classes.actions}>
+                <Button onClick={handleComplete} color="primary" variant="contained" className={classes.donezo}>
+                    {props.todo.completed ? "Mark undone" : "Donezo"}
+                </Button>
+                <Button onClick={props.handleClose} color="primary" >
                     Close
                 </Button>
                 
