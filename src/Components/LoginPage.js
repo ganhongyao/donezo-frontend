5e7f6e5739d50c6bcb042ce5a32ebee1d21fc947
ganhongyao <ganhongyao@u.nus.edu>
2021-01-19 20:30:11 +0800

    Made loginpage functional component

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,10 +1,11 @@
-import React, { Component} from 'react';
+import React, { Component, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 import { Avatar, Button, CssBaseline, TextField, withStyles } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Link } from 'react-router-dom';
 
-const useStyles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexDirection: 'column',
@@ -42,105 +43,95 @@ const useStyles = (theme) => ({
     errormessage: {
         color: 'red'
     }
-});
+}));
 
-class LoginPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          email: "",
-          password: "",
-          loginErrors: ""
-        }
-        
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+export default function LoginPage(props) {
+    const classes = useStyles();
+    const [ state, setState ] = useState({
+        email: "",
+        password: "",
+    });
+    const [loginErrors, setLoginErrors] = useState();
+
+    const handleChange = (event) => {
+        setState(prev => ({ 
+            ...prev,
+            [event.target.name]: event.target.value,
+        }))
     }
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.props.handleLoading();
+        props.handleLoading();
         axios.post("https://donezo-api.herokuapp.com/api/v1/tokens",
         {   
-                email: this.state.email,
-                password: this.state.password,                  
+                email: state.email,
+                password: state.password,                  
         })
         .then(response => {
             console.log(response)
-            this.props.handleLoading();
+            props.handleLoading();
             if (response.data.logged_in) {
-                this.props.handleLogin(response.data)
-                this.props.history.push('/home')
+                props.handleLogin(response.data)
+                props.history.push('/home')
             }
         })
         .catch(error => {
             console.log(error)
-            this.setState({loginErrors: error.response.data.errors})
-            this.props.handleLoading();
-        })}
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className={classes.root}>
-                <CssBaseline />
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <div>LOG IN</div><br/>
-                <div className={classes.errormessage}>{this.state.loginErrors}</div>
-                <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
-                    <TextField className={classes.textfield}
-                        size="small"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        type="email"
-                        autoFocus
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
-                    <TextField
-                        size="small"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}  
-                    >
-                        LOG IN
-                    </Button>    
-                </form>
-                <Link to="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                </Link>
-            </div>
-        )
+            setLoginErrors(error.response.data.errors)
+            props.handleLoading();
+        })
     }
-} 
 
-export default withStyles(useStyles)(LoginPage);
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <div>LOG IN</div><br/>
+            <div className={classes.errormessage}>{loginErrors}</div>
+            <form className={classes.form} noValidate onSubmit={handleSubmit}>
+                <TextField className={classes.textfield}
+                    size="small"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    type="email"
+                    autoFocus
+                    value={state.email}
+                    onChange={handleChange}
+                />
+                <TextField
+                    size="small"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    value={state.password}
+                    onChange={handleChange}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}  
+                >
+                    LOG IN
+                </Button>    
+            </form>
+            <Link to="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+            </Link>
+        </div>
+    )
+}
