18695e4067bb42f160fb1bd2ad4c4a79578435d0
ganhongyao <ganhongyao@u.nus.edu>
2021-01-19 10:57:42 +0800

    Fixed formatting

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom' 
 
 export default function ProtectedRoute({component: Component, isLoggedIn, ...rest}) {
-
     return (
         <Route {...rest} render={props => isLoggedIn
             ? <Component {...props} {...rest}/>
