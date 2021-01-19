907e0951f57ce3b8761e215dd8633ab7813e335c
ganhongyao <ganhongyao@u.nus.edu>
2021-01-19 20:22:13 +0800

    Fixed code formatting

--- src/Components/ZenMode.js
+++ src/Components/ZenMode.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Countdown from 'react-countdown';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Zoom } from '@material-ui/core';
 import RandomQuote from './RandomQuote';
